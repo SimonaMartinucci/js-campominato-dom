4f69a865a0ec1488ea244f36b313c928bf1d812e
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-14 15:34:19 +0200

    feat: add generator 16 numbers + add classes safe/boom

--- js/myscript.js
+++ js/myscript.js
@@ -11,8 +11,17 @@ const button = document.querySelector(".btn");
 button.addEventListener("click",
     function() {
 
+         // richiamo funzione che mi genera 16 num random
+         let bombs = arrayRandDiversi();
+
+         console.log(bombs);
+
+        //  svuoto il container ogni volta che clicco il bottone
+         container.innerHTML = "";
+
         // creo ciclo
         for(i = 1; i <= 100; i++) {
+
             // richiamo funzione per creare i box con classe
             const box = createClassElement("div", "box");
 
@@ -25,6 +34,17 @@ button.addEventListener("click",
                     box.classList.add("clicked");
                     // emetto un messaggio in console con il numero della cella cliccata
                     console.log(content);
+
+                    for(i = 1; i <= 84; i++) {
+
+                        if(!bombs.includes(content)) {
+                            box.classList.add("safe");
+                        }else {
+                            box.classList.add("boom");
+                        }
+
+                    }
+                    
                 }
             )         
             // metto i numeri all'interno dei box
